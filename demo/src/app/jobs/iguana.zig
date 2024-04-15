3f6c1a49199e3a3841a710d69f6525a72a744bb7
Bob Farrell <git@bob.frl>
2024-04-15 21:16:02 +0100

    Remove temporary test file

--- demo/src/app/jobs/iguana.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const jetzig = @import("jetzig");
-
-/// The `run` function for all jobs receives an arena allocator, a logger, and the params
-/// passed to the job when it was created.
-pub fn run(allocator: std.mem.Allocator, params: *jetzig.data.Value, logger: jetzig.Logger) !void {
-    // Job execution code
-}
