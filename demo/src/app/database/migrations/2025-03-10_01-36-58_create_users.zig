b070d281e33987b39f8d0d5d6ff5ac8e345beda8
Sean M. Collins <sean@coreitpro.com>
2025-03-10 16:32:27 -0400

    Add user table

---
+++ demo/src/app/database/migrations/2025-03-10_01-36-58_create_users.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const jetquery = @import("jetquery");
+const t = jetquery.schema.table;
+
+pub fn up(repo: anytype) !void {
+    try repo.createTable(
+        "users",
+        &.{
+            t.primaryKey("id", .{}),
+            t.column("email", .string, .{ .unique = true, .index = true }),
+            t.column("password_hash", .string, .{}),
+            t.timestamps(.{}),
+        },
+        .{},
+    );
+}
+
+pub fn down(repo: anytype) !void {
+    try repo.dropTable("users", .{});
+}
