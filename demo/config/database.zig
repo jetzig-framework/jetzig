21bf2374370927f4023d8989e4024e0c7d371b6a
Sean M. Collins <sean@coreitpro.com>
2025-03-10 21:57:02 -0400

    Shrink database.zig and comment

--- demo/config/database.zig
+++ demo/config/database.zig
@@ -1,48 +1,7 @@
 pub const database = .{
-    // Null adapter fails when a database call is invoked.
-    .development = .{
-        .adapter = .null,
-    },
+    // This configuration is used for CI
+    // in GitHub
     .testing = .{
         .adapter = .postgresql,
     },
-    .production = .{
-        .adapter = .null,
-    },
-    // PostgreSQL adapter configuration.
-    //
-    // All options except `adapter` can be configured using environment variables:
-    //
-    // * JETQUERY_HOSTNAME
-    // * JETQUERY_PORT
-    // * JETQUERY_USERNAME
-    // * JETQUERY_PASSWORD
-    // * JETQUERY_DATABASE
-    //
-    // .testing = .{
-    //     .adapter = .postgresql,
-    //     .hostname = "localhost",
-    //     .port = 5432,
-    //     .username = "postgres",
-    //     .password = "password",
-    //     .database = "myapp_testing",
-    // },
-    //
-    // .development = .{
-    //     .adapter = .postgresql,
-    //     .hostname = "localhost",
-    //     .port = 5432,
-    //     .username = "postgres",
-    //     .password = "password",
-    //     .database = "myapp_development",
-    // },
-    //
-    // .production = .{
-    //     .adapter = .postgresql,
-    //     .hostname = "localhost",
-    //     .port = 5432,
-    //     .username = "postgres",
-    //     .password = "password",
-    //     .database = "myapp_production",
-    // },
 };
