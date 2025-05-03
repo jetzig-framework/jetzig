1acb6f5df9052b113cbdccc390ddb722c8579cf0
TheJltres <joseluistresserras@gmail.com>
2025-05-03 11:04:29 +0200

    Reset config to default

--- demo/config/database.zig
+++ demo/config/database.zig
@@ -1,11 +1,11 @@
 pub const database = .{
     .development = .{
         .adapter = .postgresql,
-        .username = "root",
-        .password = "root",
+        .username = "postgres",
+        .password = "postgres",
         .hostname = "localhost",
         .database = "jetzig_demo_dev",
-        .port = 5432, // See `compose.yml`
+        .port = 14173, // See `compose.yml`
     },
     // This configuration is used for CI
     // in GitHub
