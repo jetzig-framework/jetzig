bc73e47b23ae638a1e2b4ee8053366dee3c24acc
Ben Jordan <ben@poecoh.com>
2025-08-25 00:39:10 -0400

    added hashes for aarch and darwin

--- flake.nix
+++ flake.nix
@@ -33,7 +33,12 @@
               version = "0.15.0";
               src = pkgs.fetchurl {
                 url = "https://github.com/zigtools/zls/releases/download/${version}/zls-${system}.tar.xz";
-                sha256 = "1pih3bqb89mfbmf6h0vb243z8l83j2l7vz7k0wps1lipsqzzx2sh";
+                sha256 = {
+                  "x86_64-linux" = "1pih3bqb89mfbmf6h0vb243z8l83j2l7vz7k0wps1lipsqzzx2sh";
+                  "aarch64-linux" = "1m2pamnb95vz3wvjnb31h9jnxkn2wc3aazfq7d6a7mxv5lw9271d";
+                  "x86_64-macos" = "14rx2gp45wm5zsd66hl2wgxbp6ibxk1jh978zv3xqnpgpww1ihs6";
+                  "aarch64-macos" = "0apw0pxrkwafn9pqf5pwn33v6j9ckil5y1i402bnfzpnj0qs5ivn";
+                }.${pkgs.stdenv.hostPlatform.system} or (throw "unsupported system: ${pkgs.stdenv.hostPlatform.system}");
               };
               nativeBuildInputs = [ pkgs.makeWrapper ];
               dontUnpack = true;
